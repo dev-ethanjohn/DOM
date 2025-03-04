c3e86357092c6ff0f251d757e9abb8f69647b8c2
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-03-04 10:55:30 +0800

    add lesson intro on DOM and its related technologies

---
+++ 1Intro/intro.js
@@ -0,0 +1,26 @@
+//DOM
+//DOM/ Document Object Model - is a standardized interface that describes a web page
+
+// We need to know the browser, Javascript (or any language that supports DOM tools) , and HTML/CSS
+
+//NOTE: Why need DOM
+// 1: It is part of the web standard
+// 2: Fundamental building blocks of a website
+// 3: Cross-language and cross-platform
+// 4: All frameworks built on top of the DOM
+// 5: Gives you insight in to any higher level abstractions you may use (libraries)
+//  6: Prerequisite to learning frontend frameworks and tools
+
+//NOTE: WHat DOM is not?
+// WINDOW-OBJECT (comprises all 3)
+// (1) -> BOM (Web APIs) -> ex: (1) localstorage, (2) navigator (3) location, (4) crypto (5) fetch ....https://developer.mozilla.org/en-US/docs/Web/API
+// (2) -> DOM -> EX: (1) document (2) node (3) event ....
+// (3) -> JavaScript -> ex: Object, Arrays, Functions, Maps, Sets, Generators, ...
+
+// NOTE: WHat DOM can do?
+// (1) CRUD HTML CSS on the webpage
+// (2) Change attributes of Elements on the webpage
+// (3) CRUD events that are on the webpage
+// (4) Create our own Custom Events to add to the webpage
+// (5) Create our own Custom Elements to put on the webpage
+// and more...
